4b1856e0581c7b6d5fca06d262679f2b0579a275
Pikus16 <bpikus@gmail.com>
2025-05-30 07:06:16 +0000

    update train

--- train.py
+++ train.py
@@ -61,7 +61,8 @@ def train(model, tokenizer, dataset,
           run_name: str,
           flesch_threshold: float,
           max_seq_length: int = 2048,
-          max_completion_length: int = 250):
+          max_completion_length: int = 250,
+          num_generations: int = 4):
     config = GRPOConfig(
         learning_rate=5e-6,
         adam_beta1=0.9,
@@ -73,13 +74,14 @@ def train(model, tokenizer, dataset,
         logging_steps=1,
         per_device_train_batch_size=1,
         gradient_accumulation_steps=1,
-        num_generations=4,
+        num_generations=num_generations,
         max_completion_length=max_completion_length,
         num_train_epochs=1000,
         max_grad_norm=0.1,
         report_to="wandb",
         output_dir="runs",
-        run_name=run_name
+        run_name=run_name,
+        save_steps=250
     )
     trainer = GRPOTrainer(
         model=model,
@@ -92,7 +94,7 @@ def train(model, tokenizer, dataset,
     )
     trainer.train()
     model.save_lora(f"{save_path}/lora")
-    model.save_pretrained_merged(f"{save_path}/merged", tokenizer, save_method="lora")
+    #model.save_pretrained_merged(f"{save_path}/merged", tokenizer, save_method="lora")
 
 def setup_wandb(project='GRPO_DIFFICULTY', name='gsm8k'):
     os.environ['WANDB_PROJECT'] = project
@@ -103,7 +105,8 @@ def setup_wandb(project='GRPO_DIFFICULTY', name='gsm8k'):
 @click.option('--name', type=str, default='gsm8k')
 @click.option('--save_path', type=str, default='runs')
 @click.option('--flesch_threshold', '-f', type=float, default=4, help='Flesch-Kincaid grade level threshold for reward function')
-def main(project, name, save_path, flesch_threshold):
+@click.option('--num_generations', '-n', type=int, default=4, help='Number of generations per iteration')
+def main(project, name, save_path, flesch_threshold, num_generations):
     #os.environ['WANDB_API_KEY'] = '54a50cbe22da3f857fcb7812dd80fedc2ef01ad4'
     click.echo(f'Using threshold {flesch_threshold}')
     setup_wandb(project=project, name=name)
@@ -111,7 +114,13 @@ def main(project, name, save_path, flesch_threshold):
     model, tokenizer = load_model_and_tokenizer()
     dataset = create_dataset_from_str(text=PROMPT, tokenizer=tokenizer)
 
-    train(model, tokenizer, dataset, save_path=save_path, run_name=name, flesch_threshold=flesch_threshold)
+    train(model,
+          tokenizer, 
+          dataset, 
+          save_path=save_path,
+          run_name=name,
+          flesch_threshold=flesch_threshold,
+          num_generations=int(num_generations))
 
 if __name__ == '__main__':
     main()
