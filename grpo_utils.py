1e99f3eaec2b29de726e3fbcf656fb186fb3195e
Pikus16 <bpikus@gmail.com>
2025-07-08 12:56:22 -0700

    fix error

--- grpo_utils.py
+++ grpo_utils.py
@@ -8,8 +8,10 @@ class CumulativeSuccessCallback(TrainerCallback):
 
     def on_log(self, args, state: TrainerState, control: TrainerControl, **kwargs):
         # `state.log_history[-1]` is the most recent logged metrics dict
-        num_successes = int(state.log_history[-1]['reward'] * args.num_generations)
-        self._cumulative += num_successes
-        # push to W&B yourself
-        wandb.log({"train/cumulative_successes": self._cumulative})
+        latest_state = state.log_history[-1]
+        if 'reward' in latest_state:
+            num_successes = int(latest_state['reward'] * args.num_generations)
+            self._cumulative += num_successes
+            # push to W&B
+            wandb.log({"train/cumulative_successes": self._cumulative})
         return control
