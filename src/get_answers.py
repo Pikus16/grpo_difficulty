8df4cda19766f0b96b6900434f323159416cbe86
Pikus16 <bpikus@gmail.com>
2025-08-03 18:19:00 -0700

    move getting answers to subprocess

--- src/get_answers.py
+++ src/get_answers.py
@@ -1,5 +1,7 @@
-from src_utils import do_single_run
+from src_utils import run_on_all_checkpoints, _get_checkpoint_dir
 import click
+import os
+import json
 
 @click.command()
 @click.option(
@@ -17,24 +19,30 @@ import click
 @click.option('--dataset_name', type=str, required=True)
 @click.option('--batch_size', '-b', type=int, default=16, help='Batch size to use')
 @click.option('--num_repeat',  type=int, default=10, help='Number of answers per question')
+@click.option('--run_name',  type=str, default=None, help='Run name to load adapters')
 def main(
     model_name: str,
     split: str,
     dataset_name: str,
     batch_size: int,
     num_repeat: int,
+    run_name: str
 ):
     assert split in ['train','test']
-    print(f'Running pretrained')
-    pretrained_accuracy, pretrained_passes = do_single_run(
+
+    results = run_on_all_checkpoints(
         model_name=model_name,
-        adapter_name=None,
+        num_repeat=num_repeat,
+        batch_size=batch_size,
         split=split,
         dataset_name=dataset_name,
-        batch_size=batch_size,
-        num_repeat=num_repeat
+        run_name=run_name
     )
-    print(f"Base: Accuracy: {pretrained_accuracy:0.3f}, Pass@{num_repeat}: {pretrained_passes:0.3f}")
+    if run_name is not None:
+        # save inference info
+        checkpoint_dir = _get_checkpoint_dir(dataset_name, run_name)
+        with open(os.path.join(checkpoint_dir, 'test_results.json'), 'w') as f:
+            json.dump(results, f)
 
 if __name__ == '__main__':
     main()
