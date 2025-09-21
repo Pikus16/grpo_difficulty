3fa51ed367fb550a6a786e493e4a36aab8fecc2e
Pikus16 <bpikus@gmail.com>
2025-09-20 22:39:20 -0700

    bug fix

--- src/train.py
+++ src/train.py
@@ -52,8 +52,11 @@ def create_reward_func(dataset_name, regression_reward: bool = False):
                         pred == a
                     )
             except:
-                # Set to arbitrarily large negative number
-                scores.append(-1000000)
+                if regression_reward:
+                    # Set to arbitrarily large negative number
+                    scores.append(-1000000)
+                else:
+                    scores.append(0)
         return scores.astype(int)
 
     def cruxo_reward_func(completions, answer, **kwargs):
